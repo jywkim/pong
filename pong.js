5312acd67a3799001a4d9dc3329161c3608f541d
jywkim <jkim589@gmail.com>
2018-02-16 02:07:49 -0500

    increase ball speed on collision

--- pong.js
+++ pong.js
@@ -83,6 +83,7 @@ class Pong {
     if (player.left < ball.right && player.right > ball.left &&
         player.top < ball.bottom && player.bottom > ball.top) {
         ball.vel.x = -ball.vel.x;
+        ball.vel.len *= 1.05;
     }
   }
   draw() {
